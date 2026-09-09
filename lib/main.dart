502fe698fd662b09fdd654082642ee98c38ccc8a
Renata Cardoso <renataageuscardoso@gmail.com>
2026-09-09 09:57:37 -0300

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -63,16 +63,115 @@ class _SplashScreenState extends State<SplashScreen> {
   }
 }
 
+class AppInfo{
+  final String nome;
+  final String descricao;
+  final IconData icone;
+
+  const AppInfo({
+    required this,nome,
+    required this.descricao,
+    required this,icone,
+  });
+}
+
+final List<AppInfo> meusApps=[
+  AppInfo(
+    nome: 'Calculadora de Gasolina',
+    descricao: 'Calcula litros e custo de uma viagem',
+    icone: Icons.local_gas_station,
+  ),
+  AppInfo(
+    nome: 'Calculadora de Churrasco',
+    descricao: 'Calcula carne, bebida e carvão',
+    icone: Icons.outdoor_grill,
+  )
+];
+
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text("Integra+")),
-      body: const Center(
-        child: Text('Em construção...'),
+      appBar: AppBar(title: const Text("Central de Apps")),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: [
+            DrawerHeader(
+              decoration: BoxDecoration(
+                color: Theme.of(context).colorScheme.primary,
+              ),
+              child: const Align(
+                alignment: Alignment.bottomLeft,
+                child: Text(
+                  'Central de Apps',
+                  style: TextStyle(color: Colors.white, fontSize: 20),
+                ),
+                ),
+              ),
+              ListTile(
+                leading: const Icon(Icons.home),
+                title: const Text('Início'),
+                onTap: () => Navigator.pop(context),
+              ),
+              ListTile(
+                leading: const Icon(Icons.home),
+                title: const Text('Perfil'),
+                onTap: () => Navigator.pop(context),
+              ),ListTile(
+                leading: const Icon(Icons.home),
+                title: const Text('Sair'),
+                onTap: () => Navigator.pop(context),
+              ),
+          ],
+        )
       ),
+      body: GridView.builder(
+        padding: EdgeInsets.all(16),
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          mainAxisSpacing: 12,
+          crossAxisSpacing:12,
+          childAspectRatio: 0.95,
+        ),
+        itemCount: meusApps.length,
+        itemBuilder: (context, indice){
+          final app = meusApps[indice];
+          return Card(
+            child: Padding(
+              padding: EdgeInsets.all(12),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Icon(
+                    app.icone,
+                    size: 36,
+                    color: Theme.of(context).colorScheme.primary,
+                  ),
+                  SizedBox(height: 16,),
+                  Text(
+                    app.nome,
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 13,
+                    )
+                  ),
+                  SizedBox(height: 4,),
+                  Text(
+                    app.descricao,
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontSize: 11,
+                    )
+                  ),
+                ]
+              )
+            )
+          );
+        }
+      )
     );
   }
 }
